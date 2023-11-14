686f8c77616892e9fb9aba11636c6d5e3c6835e4
MosasaUnited <mostafa447@hotmail.com>
2023-11-14 05:43:45 +0200

    DomainLayer:RefactorRemoteDataSourceThenAddContract

---
+++ lib/core/network/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
+  static const String apiKey = '6c5ee495cd0169545443e5e6bb8a639c';
+
+  static const String nowPlayingMoviePath =
+      '$baseUrl/now_playing/api_key=$apiKey';
+}
--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -3,12 +3,18 @@ import 'package:movie_clean/core/error/exceptions.dart';
 import 'package:movie_clean/core/network/error_message_model.dart';
 import 'package:movie_clean/movies/data/models/movie_model.dart';
 
+import '../../../core/network/api_constants.dart';
 import '../../domain/entities/movie.dart';
 
-class MovieRemoteDataSource {
+abstract class BaseMovieRemoteDataSource {
+  Future<List<Movie>> getNowPlayingMovies();
+  Future<List<Movie>> getPopularMovies();
+}
+
+class MovieRemoteDataSource extends BaseMovieRemoteDataSource {
+  @override
   Future<List<Movie>> getNowPlayingMovies() async {
-    final response = await Dio().get(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key=6c5ee495cd0169545443e5e6bb8a639c&language=en-US&page=1');
+    final response = await Dio().get(ApiConstants.nowPlayingMoviePath);
 
     if (response.statusCode == 200) {
       return List<MovieModel>.from((response.data['results'] as List)
@@ -18,4 +24,7 @@ class MovieRemoteDataSource {
           errorMessageModel: ErrorMessageModel.fromJson(response.data));
     }
   }
+
+  @override
+  Future<List<Movie>> getPopularMovies() {}
 }
