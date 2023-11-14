4cf8ed1d2a1fbfcf4071e46e3c5f576832dba659
MosasaUnited <mostafa447@hotmail.com>
2023-11-14 04:01:14 +0200

    DomainLayer:MovieDataSource

---
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:movie_clean/movies/data/models/movie_model.dart';
+
+import '../../domain/entities/movie.dart';
+
+class MovieRemoteDataSource {
+  Future<List<Movie>> getNowPlayingMovies() async {
+    final response = await Dio().get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=6c5ee495cd0169545443e5e6bb8a639c&language=en-US&page=1');
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data['results'] as List)
+          .map((e) => MovieModel.fromJson(e)));
+    } else {
+      return [];
+    }
+  }
+}
