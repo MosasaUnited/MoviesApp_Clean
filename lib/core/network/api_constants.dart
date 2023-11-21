25203d915b977f55186ce97eefbfc21c1e0f6a7c
MosasaUnited <mostafa447@hotmail.com>
2023-11-21 02:41:25 +0200

    MovieScreenUI

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -8,4 +8,6 @@ class ApiConstants {
       '$baseUrl/movie/popular?api_key=$apiKey';
   static const String getTopRatedMoviesPath =
       '$baseUrl/movie/top_rated?api_key=$apiKey';
+  static const String baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+  static String imageUrl(String path) => '$baseImageUrl$path';
 }
